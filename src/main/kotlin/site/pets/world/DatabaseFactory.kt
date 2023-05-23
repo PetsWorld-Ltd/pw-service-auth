297b89ef66975fabf0878d6857be27939590935f
Denis Danilchuk <ddanilchuk13@gmail.com>
2023-05-23 19:28:42 +0300

    Plugin replace kmongo InstantSerializer with own, based on strings instead of unixtime

--- src/main/kotlin/site/pets/world/DatabaseFactory.kt
+++ src/main/kotlin/site/pets/world/DatabaseFactory.kt
@@ -1,18 +1,24 @@
 package site.pets.world
 
 import com.mongodb.reactivestreams.client.MongoClient
-import io.ktor.serialization.kotlinx.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.modules.SerializersModule
-import org.litote.kmongo.Id
+import kotlinx.serialization.modules.contextual
+import kotlinx.serialization.modules.overwriteWith
 import org.litote.kmongo.coroutine.CoroutineClient
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
-import org.litote.kmongo.id.jackson.IdKeySerializer
 import org.litote.kmongo.id.serialization.IdKotlinXSerializationModule
 import org.litote.kmongo.reactivestreams.KMongo
+import org.litote.kmongo.serialization.SerializationClassMappingTypeService
 import org.litote.kmongo.serialization.kmongoSerializationModule
 import org.litote.kmongo.serialization.registerModule
-import org.litote.kmongo.serialization.registerSerializer
+import java.time.Instant
 
 class DatabaseFactory(private val serviceConfig: ServiceConfig) {
 
@@ -23,7 +29,13 @@ class DatabaseFactory(private val serviceConfig: ServiceConfig) {
 
     fun onCreate() {
         registerModule(IdKotlinXSerializationModule)
-        registerModule(kmongoSerializationModule)
+
+        registerModule(kmongoSerializationModule.overwriteWith(
+            SerializersModule {
+                contextual(Instant::class, InstantSerializer)
+            }
+        ))
+        isntallKotlinxSerializer()
         mongoClient = KMongo.createClient(serviceConfig.mongoConnectionString)
     }
 
@@ -31,13 +43,25 @@ class DatabaseFactory(private val serviceConfig: ServiceConfig) {
         return coroutineClient.getDatabase("WebAdmin")
     }
 
-    companion object {
-        const val DATABASE_WEB_ADMIN = "WebAdmin"
-        const val DATABASE_ACCAUNTS = "Accounts"
-    }
+    companion object
 }
 
+fun isntallKotlinxSerializer() {
+    System.setProperty("org.litote.mongo.mapping.service", SerializationClassMappingTypeService::class.qualifiedName!!)
+}
 
+object InstantSerializer : KSerializer<Instant> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("pw.InstantSerializer", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Instant {
+        return Instant.parse(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        encoder.encodeString(value.toString())
+    }
+
+}
 
 
 
--- src/main/kotlin/site/pets/world/admin/models/Administrator.kt
+++ src/main/kotlin/site/pets/world/admin/models/Administrator.kt
@@ -5,6 +5,8 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
+import org.litote.kmongo.Id
+import org.litote.kmongo.newId
 import java.time.Instant
 
 @Serializable
